212ddb952d43b0803de8709fc8b7287a389662e7
Aluo <green.rabite@gmail.com>
2018-06-16 21:23:57 -0700

    preload images

--- app/assets/javascripts/js/main.js
+++ app/assets/javascripts/js/main.js
@@ -1,12 +1,21 @@
-const images = ["/images/bg2.jpg", "/images/bg3.jpg", "/images/bg4.jpg", "/images/bg5.jpg"];
-images.forEach(image => {
-  preloadImage(image);
-});
-function preloadImage(url)
-{
-    var img=new Image();
-    img.src=url;
+const images = [];
+
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
 }
+
+//-- usage --//
+preload(
+    "/images/bg2.jpg",
+    "/images/bg3.jpg",
+    "/images/bg4.jpg",
+    "/images/bg5.jpg"
+);
+
+const imagesUrl=["/images/bg2.jpg","/images/bg3.jpg","/images/bg4.jpg","/images/bg5.jpg"];
 
 let imgCount = 0;
 document.addEventListener('DOMContentLoaded', doSlideshow);
@@ -14,10 +23,10 @@ const intro = document.querySelector('.intro');
 
 function doSlideshow(){
   if (imgCount >= images.length) imgCount = 0;
-  intro.style.background = `url(${images[imgCount++]}) no-repeat top center`;
+  intro.style.background = `url(${imagesUrl[imgCount++]}) no-repeat top center`;
   intro.style.backgroundSize = 'cover';
   intro.style.width = '100%';
   intro.style.position = 'relative';
-  intro.style.transition = '0.6s';
+  intro.style.transition = '1s';
   setTimeout(doSlideshow, 5000);
 }
