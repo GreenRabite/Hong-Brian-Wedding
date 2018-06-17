1951d500f6c27c2c31b235a2b081c9f73932eda8
Aluo <green.rabite@gmail.com>
2018-06-16 20:33:15 -0700

    Add js to do slideshows

---
+++ app/assets/javascripts/js/main.js
@@ -0,0 +1,15 @@
+const images = ["/images/bg2.jpg", "/images/bg3.jpg", "/images/bg4.jpg", "/images/bg5.jpg"];
+
+let imgCount = 0;
+document.addEventListener('DOMContentLoaded', doSlideshow);
+const intro = document.querySelector('.intro');
+
+function doSlideshow(){
+  if (imgCount >= images.length) imgCount = 0;
+  intro.style.background = `url(${images[imgCount++]}) no-repeat top center`;
+  intro.style.backgroundSize = 'cover';
+  intro.style.width = '100%';
+  intro.style.position = 'relative';
+  intro.style.transition = '0.6s';
+  setTimeout(doSlideshow, 5000);
+}
