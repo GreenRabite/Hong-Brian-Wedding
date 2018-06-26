22e18ae853f8f47a5b34953696612c97acdc18c0
Aluo <green.rabite@gmail.com>
2018-06-26 14:54:36 -0700

    Add event listener to update page on resize so invites don't overflow

--- frontend/components/display/invite_index.jsx
+++ frontend/components/display/invite_index.jsx
@@ -4,10 +4,22 @@ import InviteIndexItem from './invite_index_item';
 class InviteIndex extends React.Component{
   constructor(props){
     super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
   componentDidMount(){
     this.props.fetchInvites();
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillMount(){
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
   render(){
@@ -15,8 +27,10 @@ class InviteIndex extends React.Component{
     if (this.props.invites) {
       let invites = Object.values(this.props.invites);
       if (invites.length > 0) {
-        if (invites.length > 25) {
+        if (invites.length > 25 && this.state.width > 500) {
           invites = invites.slice(0,24);
+        }else if (invites.length > 15 && this.state.width < 500) {
+          invites = invites.slice(0,14);
         }
         InviteIndexItems = invites.map((invite)=>{
           return <InviteIndexItem key = {invite.id} invite={invite} />;
