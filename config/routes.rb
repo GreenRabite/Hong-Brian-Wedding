c2e67a3312fceff67900f635bdb07e1cd10401ce
Aluo <green.rabite@gmail.com>
2018-07-11 01:00:03 -0700

    setup custom routes for wedding stats api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
+  get '/getstats', to: 'stats#index'
 
   namespace :api, defaults: {format: :json} do
     resources :invites, only: [:show, :edit, :index, :update]
